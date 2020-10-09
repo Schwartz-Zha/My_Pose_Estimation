feac2af2e5a1569573ce977137f365e3f1e51a5e
billy <billy@localhost.localdomain>
2020-10-10 00:12:18 +1100

    Fourth

--- main.py
+++ main.py
@@ -39,6 +39,23 @@ def Net_Prediction(model, image, device, backbone='SimpleNet'):
     return heatmap_avg
 
 
+def draw_pose(canvas, key_points):
+    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
+               [1, 16], [16, 18], [3, 17], [6, 18]]
+
+    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
+              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
+              [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+
+    for i in range(18):
+        x = key_points[i][0]
+        y = key_points[i][1]
+        #        print("x = " + str(x) + "\n")
+        #        print("y = " + str(y) + "\n")
+        cv2.circle(canvas, (int(x), int(y)), 2, colors[i], thickness=-1)
+
+    return canvas
 
 
 
@@ -89,20 +106,7 @@ if __name__ == '__main__':
     print("find peaks in {:2.3f} seconds".format(t2 - t1))
 
 
-    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
-               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
-               [1, 16], [16, 18], [3, 17], [6, 18]]
-
-    colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0], \
-              [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255], \
-              [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
-
-    for i in range(18):
-        x = key_points[i][0]
-        y = key_points[i][1]
-#        print("x = " + str(x) + "\n")
-#        print("y = " + str(y) + "\n")
-        cv2.circle(image, (int(x), int(y)), 2, colors[i], thickness=-1)
+    canvas = draw_pose(image, key_points)
 
     print("total inference in {:2.3f} seconds".format(time.time() - since))
 
@@ -114,5 +118,4 @@ if __name__ == '__main__':
 else:
     print('PLease run this file as level 0')
     exit(1)
-
 
